617a532dc778114ba0970f359f518ae3899ff803
Julia RudenkoE <juliarudenko29@icloud.com>
2023-02-25 01:21:34 +0300

    add task3

---
+++ Task3/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("Введите число N: ");
+    var n = Convert.ToInt32(Console.ReadLine());
+
+    var numberList = new int[n + 1];
+
+    int counter = 0;
+    int length = numberList.Length;
+    while (counter < length)
+    {
+        numberList[counter] = Convert.ToInt32(Math.Pow(counter, 3));
+        counter++;
+    }
+
+    int count = numberList.Length;
+    int index = 1;
+    while (index < count)
+    {
+        Console.Write(numberList[index] + " ");
+        index++;
+    }
+    Console.ReadKey();
